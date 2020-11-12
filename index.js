8dfd5d9b5ff8a47d530639693070be0b40da3161
xhq4300966 <xhq4300966@126.com>
2020-11-12 11:37:56 +0800

    adjusted port & added gitignore

--- index.js
+++ index.js
@@ -16,9 +16,10 @@ db.items.loadDatabase();
 
 app.use(bodyParser.json());
 
-app.listen(3000, ()=> {
-    console.log('listening at localhost:3000');
-})
+const port = process.env.PORT || 3000;
+app.listen(port, ()=> {
+    console.log("starting server at" + port);
+});
 
 app.use('/', express.static('public'));
 
